24becfb7664dc98d39d899d9c6aa909a34540dc0
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-23 21:05:14 +0100

    aciklamalar eklendi

---
+++ src/test/java/tests/US_012_024_036/US_012.java
@@ -0,0 +1,21 @@
+package tests.US_012_024_036;
+
+public class US_012 {
+
+    /*
+
+        12-- Acceptance Criteria
+
+        * Cuisine types on the "https://qa.mealscenter.com/" page should be clickable.
+        * The descriptions of the stores on the opened page should include the cuisine name on the page.
+
+        12-- Test Case
+
+        * US_005 adimlari uygulanip
+          Url "https://qa.mealscenter.com/" dogrulanmali.
+        * Sayfada bulunan ilk mutfak tipine tiklanip gelen sayfada cikan mutfak isminin, Sayfada cikan ogenin aciklamasinda icerildigi dogrulanmali.
+        * Sayfada bulunan "More" dropdown una tiklanip ilk mutfak tipine tiklandiktan sonra gelen sayfada cikan mutfak isminin,
+          Sayfada cikan ogenin aciklamasinda icerildigi dogrulanmali.
+
+     */
+}
---
+++ src/test/java/tests/US_012_024_036/US_036.java
@@ -0,0 +1,23 @@
+package tests.US_012_024_036;
+
+public class US_036 {
+
+    /*
+
+        36-- Acceptance Criteria
+
+        * On the "https://qa.mealscenter.com/backoffice/admin/dashboard" page
+          Total Sales, Commission this week, Order received, Last Orders, Sales overview items should be visible.
+
+        36-- Test Case
+
+        * US_035 adimlari uygulanip
+          Url "https://qa.mealscenter.com/backoffice/admin/dashboard" dogrulanmali.
+        * Sayfada; Total Sales ogesinin gorunur oldugu dogrulanmali.  			//div[@class="report-inner"]
+        * Sayfada; Commission this week ogesinin gorunur oldugu dogrulanmali.		//div[@id="boxes"]
+        * Sayfada; Order received ogesinin gorunur oldugu dogrulanmali.			//h5[@class="m-0 text-secondary"]
+        * Sayfada; Last Orders ogesinin gorunur oldugu dogrulanmali.   			//h5[@class="m-0"]
+        * Sayfada; Sales overview ogesinin gorunur oldugu dogrulanmali.   		//h5[@class="m-0 mb-3"]
+
+     */
+}
