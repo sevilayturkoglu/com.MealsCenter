b224d77ca3e47a46e0880cfbadb59bc3e10f8709
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-24 03:04:43 +0100

    US24 de sendkeys ler gitmiyor

--- src/test/java/tests/US_012_024_036/US_024.java
+++ src/test/java/tests/US_012_024_036/US_024.java
@@ -1,5 +1,15 @@
 package tests.US_012_024_036;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.AdminPage;
+import pages.MerchantPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US_024 {
 
     /*
@@ -18,8 +28,8 @@ public class US_024 {
            Verify the url "https://qa.mealscenter.com/backoffice/food/category".
         * Verify that it works properly by clicking on the "Food" item and testing that the "Category" item is viewable.
         * Verify that there is a "Category List" item on the page that comes up after clicking on the "Category" item. (to verify that the categories are displayed)
-        * Click the "Add new" button and verify that the title of the page is "Add Category".
-        * Type "Kayseri" in the "Name" box and click the "Save" button, then click "Category" and verify that the first item in the list contains "Kayseri".
+        * Click the "Add new" button Type "Kayseri" in the "Name" box and click the "Save" button,
+          then click "Category" and verify that the first item in the list contains "Kayseri".
         * Click the "Update (symbol: pen)" button next to the "Kayseri" item and verify that the title of the page is "Add Category".
         * Type "Manti" in the "Description" box and press the "Save" button, then click "Category" and verify that the first item in the list contains "Manti".
         * Click the "Delete" button next to the "Kayseri" item and verify that the "Delete Confirmation" item is visible.
@@ -27,4 +37,38 @@ public class US_024 {
 
 
      */
+
+    @Test
+    public void MerchantAddEditDeleteCategoryTest01 () {
+
+        // Merchant ana sayfaya giris yap ve git
+        MerchantPage merchantPage = new MerchantPage();
+        merchantPage.merchantLogin();
+        ReusableMethods.bekle(1);
+
+        // food a tikla
+        Driver.getDriver().findElement(By.xpath("//li[@class='food']")).click(); // food un xpath i
+
+        // category e tikla
+        Driver.getDriver().findElement(By.xpath("//*[@id='yw0']/li[7]/ul/li[1]/a")).click(); // category nin xpath i
+        ReusableMethods.bekle(1);
+
+        Driver.getDriver().findElement(By.xpath("(//a[@type='button'])[1]")).click(); // add button un xpath i
+
+        WebElement nameBox =  Driver.getDriver().findElement(By.xpath("//label[@class='required']")); // nameBox xpath
+        nameBox.sendKeys("Nusret");
+
+        WebElement saveButton = Driver.getDriver().findElement(By.xpath("//input[@type='submit']")); //saveButton xpath
+        saveButton.click();
+
+
+
+
+
+
+
+
+
+
+    }
 }
