f53cc3d6726544c3fb5c57e95844e50b5372eb9f
S-Yucel <113521835+S-Yucel@users.noreply.github.com>
2023-03-25 23:37:17 +0300

    25.03 v2 Sariye

--- src/test/java/tests/US_005_014_015_017_029/US014_TC_001_UserProfile.java
+++ src/test/java/tests/US_005_014_015_017_029/US014_TC_001_UserProfile.java
@@ -4,24 +4,24 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import pages.UserPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
-public class US_005_TC_001_UserLogin {
+
+
+public class US014_TC_001_UserProfile {
+
 
     UserPage userPage = new UserPage();
 
     @Test
-    public void userLoginPozitifTest() {
-
-        //https://qa.mealscenter.com/account/login
-        // accept cookies
-        //Verify  login page:
-        //userLoginPage.usernameEmailBox.click();
+    public void userProfileTest() {
 
         Driver.getDriver().get(ConfigReader.getProperty("userUrl"));
+
+
         if (userPage.userCookies.isDisplayed()) {
 
             userPage.userCookies.click();
@@ -34,7 +34,24 @@ public class US_005_TC_001_UserLogin {
 
             actions.click(userPage.userSignInLogin).perform();
 
-            Driver.closeDriver();
+            actions.moveToElement(userPage.userUstDropDownButton).click();
+
+            ReusableMethods.bekle(2);
+
+            actions.sendKeys(Keys.ARROW_DOWN).perform();
+
+            ReusableMethods.bekle(2);
+
+            actions.sendKeys(Keys.ENTER).perform();
+
+            String actualUrl = Driver.driver.getCurrentUrl();
+            String expectedUrl = "https://qa.mealscenter.com/account/profile";
+
+
+            Assert.assertEquals(actualUrl,expectedUrl,"profile page is active");
+
+
+            //Driver.closeDriver();
 
 
             //if no cookies
@@ -47,15 +64,19 @@ public class US_005_TC_001_UserLogin {
                     .sendKeys(ConfigReader.getProperty("userLoginPasswordSariye")).perform();
             actions.click(userPage.userSignInLogin).perform();
 
+            actions.moveToElement(userPage.userUstDropDownButton).click();
 
+            ReusableMethods.bekle(2);
 
-            // //Verify that the user is logged in and redirected
-            // to their account dashboard
+            actions.sendKeys(Keys.ARROW_DOWN).perform();
 
-            Assert.assertTrue(userPage.userSignIn.isDisplayed());
+            ReusableMethods.bekle(2);
+
+            actions.sendKeys(Keys.ENTER).perform();
 
             Driver.closeDriver();
         }
+
 
     }
 
