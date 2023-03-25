0f2b174ae97cb64001adbafcafbc63b56884ac77
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-25 18:08:39 +0100

    25/08/23

--- src/test/java/tests/US_012_024_036/US_012.java
+++ src/test/java/tests/US_012_024_036/US_012.java
@@ -10,12 +10,10 @@ public class US_012 {
         US_012  As a user, I would like to have a page with restaurants of different local cuisines.
 
         12-- Acceptance Criteria
-
         * Cuisine types on the "https://qa.mealscenter.com/" page should be clickable.
         * The descriptions of the stores on the opened page should include the cuisine name on the page.
 
         12-- Test Case
-
         * Follow the US_005 steps and
            The url "https://qa.mealscenter.com/" must be verified.
          * It should be verified that the name of the cuisine that appears on the page that comes up after clicking on the Cuisine type
---
+++ src/test/java/tests/US_012_024_036/US_024.java
@@ -0,0 +1,24 @@
+package tests.US_012_024_036;
+
+public class US_024 {
+
+    /*
+        24-- Acceptance Criteria
+        * On the "https://qa.mealscenter.com/backoffice/food/category" page.
+          Categories should be visible.
+        * Adding category, deleting category and editing category should be possible
+        * Changes made (such as adding, deleting and editing) should be visible.
+
+        24-- Test Case
+        * US_019 adimlari uygulanip
+          Url "https://qa.mealscenter.com/backoffice/food/category" dogrulanmali.
+        * "Food" ogesine tiklanilmali ve "Category" ogesinin goruntulenebilir oldugu test edilerek uygun sekilde calistigi dogrulanmali.
+        * "Category" ogesine tiklanip gelen sayfada "Category List" ogesinin oldugu dogrulanmali. (kategorilerin goruntulendigini dogrulamak icin)
+        * "Add new" butonuna tikla ve gelen sayfanin title nin "Add Category" oldugunu dogrula.
+        * "Name" kutusuna "yeni menu" yaz ve "Save" butonuna bas sonrasinda "Category" ogesine tikla ve listedeki ilk elemanin "yeni menu" icerdigini dogrula.
+        * "yeni menu" ogesinin yaninda bulunan "Update (kalem isareti)" butonuna tikla ve gelen sayfanin title nin "Add Category" oldugunu dogrula.
+        * "Description" kutusuna "aciklama" yaz ve "Save" butonuna bas sonrasinda "Category" ogesine tikla ve listedeki ilk elemanin "aciklama" icerdigini dogrula.
+        * "yeni menu" ogesinin yaninda bulunan "Delete" butonuna tikla ve "Delete Confirmation" ogesinin gorunur oldugunu dogrula.
+        * "Delete" butonuna tikla ve listedeki ilk elemanin "yeni menu" icermedigini dogrula.
+     */
+}
