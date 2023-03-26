01177b6c4ae49880a944f5a15ae8d350b77d16d1
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-26 22:20:42 +0200

    US24 de calisiyor ama raporlamalar halen yok

--- src/test/java/tests/US_012_024_036/US_024.java
+++ src/test/java/tests/US_012_024_036/US_024.java
@@ -1,5 +1,15 @@
 package tests.US_012_024_036;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.AdminPage;
+import pages.MerchantPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US_024 {
 
     /*
@@ -8,6 +18,11 @@ public class US_024 {
           Categories should be visible.
         * Adding category, deleting category and editing category should be possible
         * Changes made (such as adding, deleting and editing) should be visible.
+
+        Bir restaurant yöneticisi olarak (restaurantima ait category'leri görüntüleyebilmek,
+        yeni bir category ekleyebilmek , duzenleyebilmek ve silebilmek icin) Category List sayfasinin bulunmasini istiyorum.
+
+        backoffice/food/category
 
         24-- Test Case
         * US_019 adimlari uygulanip
@@ -21,4 +36,65 @@ public class US_024 {
         * "yeni menu" ogesinin yaninda bulunan "Delete" butonuna tikla ve "Delete Confirmation" ogesinin gorunur oldugunu dogrula.
         * "Delete" butonuna tikla ve listedeki ilk elemanin "yeni menu" icermedigini dogrula.
      */
+
+    @Test
+    public void addEditDeleteCategoriesTest () {
+
+        MerchantPage merchantPage = new MerchantPage();
+        merchantPage.merchantLogin();
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        Driver.getDriver().findElement(By.xpath("//li[@class='food']")).click();  // food un locator u
+        Driver.getDriver().findElement(By.xpath("//li[@class='position-relative food_category']")).click(); //categorynin locator u
+        Driver.getDriver().findElement(By.xpath("(//a[@type='button'])[1]")).click(); // add button locator u
+
+        Driver.getDriver().findElement(By.xpath("//label[@class='required']")).click(); // nameBox in locatoru
+
+        actions.sendKeys("Kayseri").perform();
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        ReusableMethods.bekle(1);
+
+        WebElement saveButton = Driver.getDriver().findElement(By.cssSelector("input[value='Save']"));  // save button locator
+        saveButton.click();
+
+        Driver.getDriver().findElement(By.xpath("//li[@class='position-relative food_category active']")).click(); //categorynin locator u
+
+        Driver.getDriver().findElement(By.xpath("(//a[@data-toggle='tooltip'])[1]")).click(); // edit button
+
+        Driver.getDriver().findElement(By.xpath("//textarea[@placeholder='Description']")).click(); // description un locator u
+
+        actions.sendKeys("Manti").perform();
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        ReusableMethods.bekle(1);
+
+        Driver.getDriver().findElement(By.xpath("//input[@name='yt0']")).click(); // save butonunun locator u
+
+        Driver.getDriver().findElement(By.xpath("//li[@class='position-relative food_category active']")).click(); //categorynin locator u
+
+        Driver.getDriver().findElement(By.cssSelector("tbody tr:nth-child(1) td:nth-child(3) div:nth-child(1) a:nth-child(2) i:nth-child(1)")).click(); // delete kutusunun locator u
+
+        ReusableMethods.bekle(1);
+
+        Driver.getDriver().findElement(By.cssSelector(".btn.btn-green.item_delete")).click(); // cookie deki delete butonunun locator u
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 }
