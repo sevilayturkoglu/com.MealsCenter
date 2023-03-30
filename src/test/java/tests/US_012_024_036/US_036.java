79567fece5f0909d6d52f36f96b903cf045d5387
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-30 16:55:22 +0200

    last commit

--- src/test/java/tests/US_012_024_036/US_036.java
+++ src/test/java/tests/US_012_024_036/US_036.java
@@ -8,25 +8,6 @@ import utilities.TestBaseReport;
 
 public class US_036 extends TestBaseReport {
 
-    /*
-        US_036  As an admin, I should be able to see summary information on the Dashboard page
-
-        36-- Acceptance Criteria
-        * On the "https://qa.mealscenter.com/backoffice/admin/dashboard" page
-          Total Sales, Commission this week, Order received, Last Orders, Sales overview items should be visible.
-
-        36-- Test Case
-        * Follow the US_035 steps and
-           Verify url "https://qa.mealscenter.com/backoffice/admin/dashboard".
-        * On the page; Verify that the Total Sales item is visible.		//div[@class="report-inner"]
-        * On the page; Verify that the Commission this week item is visible.	//div[@id="boxes"]
-        * On the page; Verify that Order received is visible.			//h5[@class="m-0 text-secondary"]
-        * On the page; Verify that the Last Orders item is visible.		//h5[@class="m-0"]
-        * On the page; Verify that the Sales overview item is visible.		//h5[@class="m-0 mb-3"]
-
-        TC_00203_AfterOrderCartBagTest()
-     */
-
     @Test
     public void Test01() {
 
